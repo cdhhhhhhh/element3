74864a0f10c697b4fc1f1b1eebe0cfaaffe07ee7
cdh <541187580@qq.com>
2020-09-06 02:24:28 +0800

    add

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -164,9 +164,9 @@
 </template>
 
 <script type="text/babel">
-import Emitter from 'element-ui/src/mixins/emitter'
-import Focus from 'element-ui/src/mixins/focus'
-import Locale from 'element-ui/src/mixins/locale'
+import useEmitter from 'element-ui/src/use/emitter'
+import useFocus from 'element-ui/src/use/focus'
+import useLocale from 'element-ui/src/use/locale'
 import ElInput from 'element-ui/packages/input'
 import ElSelectMenu from './select-dropdown.vue'
 import ElOption from './option.vue'
@@ -188,32 +188,66 @@ import {
 } from 'element-ui/src/utils/util'
 import NavigationMixin from './navigation-mixin'
 import { isKorean } from 'element-ui/src/utils/shared'
-
+import { inject ,provide,computed,toRefs,onMounted,reactive} from 'vue'
 export default {
-  mixins: [Emitter, Locale, Focus('reference'), NavigationMixin],
-  emits:[],
-  setup(props,{emit}){
+  mixins: [NavigationMixin],
+  emits: [],
+  setup(props, { emit }) {
+    const elForm = inject('elForm')
+    const elFormItem = inject('elFormItem')
+    provide('select',this)
 
+    const state = reactive({
+      options: [],
+      cachedOptions: [],
+      createdLabel: null,
+      createdSelected: false,
+      selected: this.multiple ? [] : {},
+      inputLength: 20,
+      inputWidth: 0,
+      initialInputHeight: 0,
+      cachedPlaceHolder: '',
+      optionsCount: 0,
+      filteredOptionsCount: 0,
+      visible: false,
+      softFocus: false,
+      selectedLabel: '',
+      hoverIndex: -1,
+      query: '',
+      previousQuery: null,
+      inputHovering: false,
+      currentPlaceholder: '',
+      menuVisibleOnFocus: false,
+      isOnComposition: false,
+      isSilentBlur: false
+    })
+
+
+
+
+    return {
+
+    }
   },
   name: 'ElSelect',
 
   componentName: 'ElSelect',
 
-  inject: {
-    elForm: {
-      default: ''
-    },
+  // inject: {
+  //   elForm: {
+  //     default: ''
+  //   },
+  //
+  //   elFormItem: {
+  //     default: ''
+  //   }
+  // },
 
-    elFormItem: {
-      default: ''
-    }
-  },
-
-  provide() {
-    return {
-      select: this
-    }
-  },
+  // provide() {
+  //   return {
+  //     select: this
+  //   }
+  // },
 
   computed: {
     _elFormItemSize() {
@@ -364,32 +398,32 @@ export default {
     }
   },
 
-  data() {
-    return {
-      options: [],
-      cachedOptions: [],
-      createdLabel: null,
-      createdSelected: false,
-      selected: this.multiple ? [] : {},
-      inputLength: 20,
-      inputWidth: 0,
-      initialInputHeight: 0,
-      cachedPlaceHolder: '',
-      optionsCount: 0,
-      filteredOptionsCount: 0,
-      visible: false,
-      softFocus: false,
-      selectedLabel: '',
-      hoverIndex: -1,
-      query: '',
-      previousQuery: null,
-      inputHovering: false,
-      currentPlaceholder: '',
-      menuVisibleOnFocus: false,
-      isOnComposition: false,
-      isSilentBlur: false
-    }
-  },
+  // data() {
+  //   return {
+  //     options: [],
+  //     cachedOptions: [],
+  //     createdLabel: null,
+  //     createdSelected: false,
+  //     selected: this.multiple ? [] : {},
+  //     inputLength: 20,
+  //     inputWidth: 0,
+  //     initialInputHeight: 0,
+  //     cachedPlaceHolder: '',
+  //     optionsCount: 0,
+  //     filteredOptionsCount: 0,
+  //     visible: false,
+  //     softFocus: false,
+  //     selectedLabel: '',
+  //     hoverIndex: -1,
+  //     query: '',
+  //     previousQuery: null,
+  //     inputHovering: false,
+  //     currentPlaceholder: '',
+  //     menuVisibleOnFocus: false,
+  //     isOnComposition: false,
+  //     isSilentBlur: false
+  //   }
+  // },
 
   watch: {
     selectDisabled() {
